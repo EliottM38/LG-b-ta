cadd25379b5cf5b3b6caddde397cd255fa2a1478
Eliott <eliott.menuge@outlook.fr>
2026-06-06 14:36:35 +0200

    Add files via upload

--- api/config.js
+++ api/config.js
@@ -3,6 +3,6 @@ export default function handler(req, res) {
   res.setHeader('Cache-Control', 'no-store');
   res.send(`
     window.SUPABASE_URL = "${process.env.SUPABASE_URL || ''}";
-    window.SUPABASE_KEY = "${process.env.SUPABASE_ANON_KEY || ''}";
+    window.SUPABASE_KEY = "${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''}";
   `);
 }
